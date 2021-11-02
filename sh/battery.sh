25e75c1a64bb963e508ed83f76fee8e6e4298c27
Stepan <ashencz@gmail.com>
2021-11-02 13:08:46 +0100

    Show a hyphen for battery charge if the device is not connected

--- sh/battery.sh
+++ sh/battery.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-amixer -D bluealsa | tail -n 1 | sed -E 's/.*\[(.*)\].*/\1/g'
+BAT=$(amixer -D bluealsa | tail -n 1 | sed -E 's/.*\[(.*)\].*/\1/g')
+
+if [ $BAT -z ]
+then
+	echo '-'
+else
+	echo $BAT
+fi
